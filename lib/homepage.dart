6932d35c9e6fc9804e1328caf97be687e4ffad7f
Cwoodard2 <cawooda@siue.edu>
2023-07-20 00:11:04 -0500

    Added ability to change current page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './timerecorder.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key, required this.title});
@@ -10,21 +11,50 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = SizedBox(width: 500, height: 500, child: TheHomepage());
+        break;
+      case 1:
+        page = SizedBox(width: 500, height: 500, child: TimeRecorderPage(title: 'Time recorder'));
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
     return Scaffold(
-        body: Center(
-      child: NavigationBar(
-        destinations: [
-          NavigationDestination(
-          icon: Icon(Icons.home), 
-          label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.list), 
-            label: "Activity Logger")
-        ],
-      ),
+        body: Column(
+      children: [
+        NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.list), label: "Activity Logger")
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+        ),
+        Container(
+          child: page,
+        )
+      ],
     ));
+  }
+}
+
+class TheHomepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("this is the homepage");
   }
 }
